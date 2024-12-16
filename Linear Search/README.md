d1e85fbbd9ac52fc078a03a136e7f41f9d8fd01b
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-12-16 10:18:40 -0300

    linear-js

---
+++ Linear Search/README.md
@@ -0,0 +1,4 @@
+#Linear search
+
+Linear search, also known as sequential search, is an algorithm that searches for a specific value in a list of elements, checking each one one by one.
+
--- Linear Search/script.js
+++ Linear Search/script.js
@@ -1,1 +1,17 @@
-console.info("Hello, World!");
+var arr = [1,2,3,4,5];
+
+var number = 4;
+
+function Linear(arr, number) {
+
+    for(var i =0; i < arr.length; i++){
+        if(number == arr[i]){
+            return true
+        }
+    }
+    return false
+}
+
+var result = Linear(arr, number)
+
+console.info(result)
