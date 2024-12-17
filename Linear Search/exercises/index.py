212c61dda902a2cd6e856c49e68f6d57fcc82612
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-12-17 13:37:34 -0300

    python

--- Linear Search/exercises/index.py
+++ Linear Search/exercises/index.py
@@ -1,8 +1,23 @@
-# make with class
+class linear_search:
+
+    def __init__(self, arr, number):
+
+        self.arr = arr
+        self.number = number
+
+    
+    def busca_linear_invertida(arr, number):
+
+        for i in range(len(arr) - 1, -1, -1):
+
+            if arr[i] == number:
+                return print(True)
+        print(False)
 
 
 
+arr = [1,2,3,4]
 
+number = 3
 
-
-print("Teste")
+linear_search.busca_linear_invertida(arr,number)
