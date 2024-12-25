8ff809180b312fdbe6f50dded6213482e4c54272
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-12-25 11:47:58 -0300

    stack - LIFO

--- stack/script.py
+++ stack/script.py
@@ -1,19 +1,18 @@
 
 def stack(arr, item):
 
-    print(f" Array origin: {arr} ",  end="\n")
+    print(f"Array origin: {arr} ",  end="\n")
 
     arr.append(item)
 
-    print(f" Array with append: {arr} ",  end="\n")
+    print(f"Array with append: {arr} ",  end="\n")
 
     arr.pop()
 
-    print(f"Array with remove the end element? {arr}", end="\n")
+    print(f"Array with remove the end element: {arr}\n")
 
 arr = [1,2,3,4]
 
 item = 5
 
 stack(arr, item)
-
