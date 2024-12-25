9d2b2ca50949f352ee284bd2cedbb8b39cfc978e
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-12-25 11:35:28 -0300

    lifo - stack

---
+++ stack/script.py
@@ -0,0 +1,21 @@
+
+def stack(arr, item):
+
+    print(f" Array origin: {arr} ",  end="\n")
+
+    arr.append(item)
+
+    print(f" Array with append: {arr} ",  end="\n")
+
+    arr.pop()
+
+    print(f"Array with remove the end element? {arr}", end="\n")
+
+
+
+arr = [1,2,3,4]
+
+item = 5
+
+stack(arr, item)
+
