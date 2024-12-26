84c90454ed4358bbcd89b7386d3820d85f0f7d0b
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-12-26 11:13:01 -0300

    sort

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-arr = [1,2,3,4]
+arr = [3,2,5,4,1,0]
 
-print(arr.search(2))
+for i in range(len(arr) - 1):
+    index = i
+    for j in range(i + 1, len(arr)):
+        if arr[j] < arr[index]:
+            index = j
+    arr[i], arr[index] = arr[index], arr[i]
 
+print(arr)
